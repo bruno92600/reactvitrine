b578f31ce1ad5a20b442d04fa7c0d1208fb2cb83
Pascoal Goncalves Bruno <pascoal.goncalves.bruno@gmail.com>
2022-03-26 20:02:30 +0100

    envoie

---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+
+const Contact = () => {
+  return (
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+      </div>
+    </main>
+  );
+};
+
+export default Contact;
